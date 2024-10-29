e6ed484a88c7e5d42fbc022ba55cbefd8d3b4368
friedrich-de <fde@tuta.com>
2024-10-29 19:56:21 +0100

    Unblock sending DB

--- cogs/dumb_db.py
+++ cogs/dumb_db.py
@@ -3,10 +3,19 @@ import gzip
 import shutil
 import tempfile
 import discord
+import asyncio
 from discord.ext import commands
 from lib.bot import TMWBot
 
 PATH_TO_DB = os.getenv("PATH_TO_DB", "data/db.sqlite3")
+
+
+def create_temporary_gzip_file():
+    temp_file_path = os.path.join(tempfile.gettempdir(), "db.sqlite3.gz")
+    with open(PATH_TO_DB, 'rb') as f_in:
+        with gzip.open(temp_file_path, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    return temp_file_path
 
 
 class DatabasePoster(commands.Cog):
@@ -16,12 +25,8 @@ class DatabasePoster(commands.Cog):
     @discord.app_commands.command(name="post_db", description="Gzip the database file and post it to the channel.")
     async def post_db(self, interaction: discord.Interaction):
         await interaction.response.defer()
-        temp_file_path = os.path.join(tempfile.gettempdir(), "db.sqlite3.gz")
         try:
-            with open(PATH_TO_DB, 'rb') as f_in:
-                with gzip.open(temp_file_path, 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
-
+            temp_file_path = await asyncio.to_thread(create_temporary_gzip_file)
             await interaction.followup.send(file=discord.File(temp_file_path, filename="db.sqlite3.gz"))
         except Exception as e:
             await interaction.followup.send(f"An error occurred: {e}"[:2000])
